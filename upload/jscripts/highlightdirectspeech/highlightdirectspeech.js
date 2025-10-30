bcafa81f7637d2167cbd595f6a6e6ba55cff4bf6
Katja <47885564+katjalennartz@users.noreply.github.com>
2025-10-30 10:14:26 +0100

    Rename 'directspeech' class to 'highlight-speach'

--- upload/jscripts/highlightdirectspeech/highlightdirectspeech.js
+++ upload/jscripts/highlightdirectspeech/highlightdirectspeech.js
@@ -6,7 +6,7 @@ function highlightDirectSpeechInPosts() {
   if (currentState === 'on') {
     $caption.data('state', 'off');
     $caption.text('highlight "abc"');
-    $('.post_body').find('span.directspeech').contents().unwrap();
+    $('.post_body').find('span.highlight-speach').contents().unwrap();
   }
   else {
     $caption.data('state', 'on');
@@ -16,15 +16,15 @@ function highlightDirectSpeechInPosts() {
 
     $('.post_body').each(function () {
       //spans entfernen
-      $(this).find('span.directspeech').contents().unwrap();
+      $(this).find('span.highlight-speach').contents().unwrap();
 
       let body_str = $(this).html();
-      body_str = body_str.replace(quoteRegex, '<span class="directspeech">$1</span>');
+      body_str = body_str.replace(quoteRegex, '<span class="highlight-speach">$1</span>');
 
       // korrektur html tags
       body_str = body_str.replace(
-        /(<span class="directspeech">)([^<>]*)(<[^>]+>+)([^<>]*)(<\/span>)/g,
-        '$1$2</span>$3<span class="directspeech">$4$5'
+        /(<span class="highlight-speach">)([^<>]*)(<[^>]+>+)([^<>]*)(<\/span>)/g,
+        '$1$2</span>$3<span class="highlight-speach">$4$5'
       );
 
       $(this).html(body_str);
